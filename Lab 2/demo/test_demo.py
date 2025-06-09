5145cb0bb7304ff22729eeab2e8106aefabdde9f
Nagy Tamas <nagy97tamas@gmail.com>
2025-06-09 14:56:03 +0300

    (Lab 2) Done 1.2

--- Lab 2/demo/test_demo.py
+++ Lab 2/demo/test_demo.py
@@ -7,7 +7,7 @@ LOGGER = logging.getLogger(__name__)
 def test_deposit():
     LOGGER.info(f"Running deposit test.")
     account = BankAccount("Dagobert Duck", (12345678, 87654321, 00000000), 10)
-    
+
     account.deposit(5)
     LOGGER.debug(account)
     assert account.balance == 15
@@ -16,7 +16,7 @@ def test_deposit():
 def test_withdraw():
     LOGGER.info(f"Running withdraw test.")
     account = BankAccount("Dagobert Duck", (12345678, 87654321, 00000000), 10)
-    
+
     account.withdraw(5)
     LOGGER.debug(account)
     assert account.balance == 5
@@ -25,16 +25,16 @@ def test_withdraw():
 def test_is_overdrawn_positive():
     LOGGER.info(f"Running withdraw test.")
     account = BankAccount("Dagobert Duck", (12345678, 87654321, 00000000), 10)
-    
+
     account.withdraw(5)
     LOGGER.debug(account)
     assert account.is_overdrawn() == False
-    
+
 
 def test_is_overdrawn_negative():
     LOGGER.info(f"Running withdraw test.")
     account = BankAccount("Dagobert Duck", (12345678, 87654321, 00000000), 10)
-    
+
     account.withdraw(15)
     LOGGER.debug(account)
     assert account.is_overdrawn() == True
